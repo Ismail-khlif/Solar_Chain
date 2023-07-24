62569e2d70f9a626cc1b305c2446e6d46210f6d9
Yessin Khlif <yessin.khlif@esprit.tn>
2023-07-24 09:46:18 +0100

    Create Campaign Contract and instance many version of it

--- Solar_chain/ethereum/contracts/Project.sol
+++ Solar_chain/ethereum/contracts/Project.sol
@@ -2,20 +2,28 @@ pragma solidity ^0.8.20;
 
 contract Campaign {
     struct CampaignInfo {
-        address projectOwner; uint value; address recipient;
+        address projectOwner;
+        uint consomationYear;
+        address recipient;
+        string description;
+        uint limiteconsomation;
+        uint joursOffre;
     }
     
     CampaignInfo[] public campaigns;
     
-    constructor(string memory , uint256 ) {
-        createCampaign(, );
+    constructor(address recipient , uint consomationYear, string memory description, uint limiteconsomation, uint joursOffre ) {
+        createCampaign(msg.sender, consomationYear, recipient, description, limiteconsomation, joursOffre);
     }
     
-    function createCampaign(address projectOwner, uint value, address recipient) public {
+    function createCampaign(address projectOwner, uint consomationYear, address recipient, string memory description, uint limiteconsomation, uint joursOffre) public {
         CampaignInfo memory newCampaign = CampaignInfo({
             projectOwner: projectOwner,
-            value: value,
-            recipient: recipient
+            consomationYear: consomationYear,
+            recipient: recipient,
+            description: description,
+            limiteconsomation: limiteconsomation,
+            joursOffre : joursOffre
         });
         campaigns.push(newCampaign);
     }

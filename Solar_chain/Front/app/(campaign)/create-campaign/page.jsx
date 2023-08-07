544604bd3ccad97c08aed596368cff9a534d2490
Maram Arfaoui <92606780+maramarfaoui@users.noreply.github.com>
2023-08-07 15:52:57 +0200

    react integration front

---
+++ Solar_chain/Front/app/(campaign)/create-campaign/page.jsx
@@ -0,0 +1,123 @@
+"use client"
+import React, { useState,useContext } from 'react'
+import { useRouter } from 'next/navigation';
+import { ethers } from 'ethers';
+
+import {StateContext} from '@/context/Context';
+// import { useStateContext } from '@/context/Context';
+import { money } from '@/public/assets';
+import  CustomButton from '@/components/Button';
+import  FormField from '@/components/FormField';
+import  Loader from '@/components/Loader';
+import { checkIfImage } from '@/lib/utils';
+import Image from 'next/image.js';
+
+const CreateCampaign = () => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false);
+  const {createCampaign} = useContext(StateContext)
+  const [form, setForm] = useState({
+    name: '',
+    title: '',
+    description: '',
+    target: '', 
+    deadline: '',
+    image: ''
+  });
+
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    checkIfImage(form.image, async (exists) => {
+      if(exists) {
+        setIsLoading(true)
+        await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
+        setIsLoading(false);
+        router.push('/');
+      } else {
+        alert('Provide valid image URL')
+        setForm({ ...form, image: '' });
+      }
+    })
+  }
+
+  return (
+    <div className="max-w-5xl mx-auto flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4 flex-1">
+      {isLoading && <Loader />}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
+      </div>
+
+      <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField 
+            labelName="Your Name *"
+            placeholder="John Doe"
+            inputType="text"
+            value={form.name}
+            handleChange={(e) => handleFormFieldChange('name', e)}
+          />
+          <FormField 
+            labelName="Campaign Title *"
+            placeholder="Write a title"
+            inputType="text"
+            value={form.title}
+            handleChange={(e) => handleFormFieldChange('title', e)}
+          />
+        </div>
+
+        <FormField 
+            labelName="Story *"
+            placeholder="Write your story"
+            isTextArea
+            value={form.description}
+            handleChange={(e) => handleFormFieldChange('description', e)}
+          />
+
+        <div className="w-full flex justify-start items-center p-4 bg-green-600 h-[120px] rounded-[10px]">
+          <Image src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">You will get 98% of the raised amount</h4>
+        </div>
+
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField 
+            labelName="Goal *"
+            placeholder="ETH 0.50"
+            inputType="text"
+            value={form.target}
+            handleChange={(e) => handleFormFieldChange('target', e)}
+          />
+          <FormField 
+            labelName="End Date *"
+            placeholder="End Date"
+            inputType="date"
+            value={form.deadline}
+            handleChange={(e) => handleFormFieldChange('deadline', e)}
+          />
+        </div>
+
+        <FormField 
+            labelName="Campaign image *"
+            placeholder="Place image URL of your campaign"
+            inputType="url"
+            value={form.image}
+            handleChange={(e) => handleFormFieldChange('image', e)}
+          />
+
+          <div className="flex justify-center items-center mt-[40px]">
+            <CustomButton 
+              btnType="submit"
+              title="Submit new campaign"
+              styles="bg-sky-600"
+            />
+          </div>
+      </form>
+    </div>
+  )
+}
+
+export default CreateCampaign
---
+++ Solar_chain/Front/app/profile/page.jsx
@@ -0,0 +1,33 @@
+"use client"
+import React, { useState, useEffect,useContext } from 'react'
+
+import DisplayCampaigns from '@/components/DisplayCampaigns';
+import { StateContext } from '@/context/Context'
+
+const Profile = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getUserCampaigns } = useContext(StateContext);
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns 
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  )
+}
+
+export default Profile
